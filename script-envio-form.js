bffaaca90bd49c9fd8958183b36c02d3ad008c29
Leonardo Rodrigues dos Santos <96453698+lrodriguesdossantos@users.noreply.github.com>
2026-02-11 20:34:05 -0300

    Refactor sendForm method for improved error handling

--- script-envio-form.js
+++ script-envio-form.js
@@ -27,28 +27,32 @@ class FormSubmit {
         return formObject;
     }
 
-   async sendForm(event) {
-    event.preventDefault();
+    async sendForm(event) {
+        event.preventDefault();
 
-    try {
-        const response = await fetch(this.url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-            body: JSON.stringify(this.getFormObject()),
-        });
+        try {
+            const response = await fetch(this.url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify(this.getFormObject()),
+            });
 
-        const data = await response.json();
-   if (data.success === "true" || data.success === true) {
-    this.displaySuccess();
-    setTimeout(() => {
-        window.location.href = "index.html";
-    }, 2000);
-       }
-       catch (error) {
-        this.displayError();
+            const data = await response.json();
+
+            if (data.success === "true" || data.success === true) {
+                this.displaySuccess();
+                setTimeout(() => {
+                    window.location.href = "index.html";
+                }, 2000);
+            } else {
+                this.displayError();
+            }
+
+        } catch (error) {
+            this.displayError();
+        }
     }
 }
- 
